2913c954251e828985890cca13975ab839e5d0c3
emanuel <raademang@gmail.com>
2017-04-18 10:26:23 -0400

    real world distances

--- imageprocessing/eggDetection.py
+++ imageprocessing/eggDetection.py
@@ -1,18 +1,23 @@
 import cv2
 import numpy as np
-import time
+
+from imageprocessing.pixelDistance import distanceBetweenPointsPixel, distanceToRealWorld
+from utils.timer import Timer
 
 # Blob detection method
 
 lower_white = np.array([0, 0, 150], dtype=np.uint8)
 upper_white = np.array([180, 255, 255], dtype=np.uint8)
 
+center_y = 480/2
+center_x = 640/2
+
 def findEgg(img):
     # Filter out white
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(imgHSV, lower_white, upper_white)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+    mask = cv2.erode(mask, None, iterations=1)
+    mask = cv2.dilate(mask, None, iterations=1)
 
     # Apply mask
     #filtered = cv2.bitwise_and(img.copy(), img.copy(), mask=mask)
@@ -61,14 +66,11 @@ def findEggDebug(img):
     params.filterByColor = True
     params.blobColor = 255
 
-    params.filterByCircularity = False
-
     params.filterByConvexity = True
     params.minConvexity = 0.9
-    """
+
     params.filterByCircularity = True
     params.minCircularity = 0.8
-    """
 
     detector = cv2.SimpleBlobDetector_create(params)
     keypoints = detector.detect(filteredGray)
@@ -78,30 +80,56 @@ def findEggDebug(img):
 def main():
     #img = cv2.imread("../img/egg3.jpg")
 
+    timer = Timer()
     cap = cv2.VideoCapture(0)
 
     while True:
         ret, img = cap.read()
-
         keypoints, f = findEggDebug(img)
 
-        start_time = time.time()
-        imgk = cv2.drawKeypoints(img, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        # print("--- %s seconds ---" % (time.time() - start_time))
+        if len(keypoints) > 0:
+            print 'ok'
+            # imgk = cv2.drawKeypoints(img, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-        # Draw centroids
-        for k in keypoints:
-            x = int(k.pt[0])
-            y = int(k.pt[1])
-            cv2.circle(imgk, (x, y), 10, (0, 0, 255), -1)
+            # Loops through every detection, and finds the distance to the center
+            distance = []
+            for j in range(len(keypoints)):
+                x = int(keypoints[j].pt[0])
+                y = int(keypoints[j].pt[1])
+                distance.append(abs(distanceBetweenPointsPixel(center_x, center_y, x, y)))
 
-        cv2.imshow("filtered", f)
-        # cv2.imshow("mask", mask)
-        cv2.imshow("img_keypoints", imgk)
+            # Finds the shortest distance to the center of the frame
+            shortestDistanceIndex = distance.index(min(distance))
+            x = int(keypoints[shortestDistanceIndex].pt[0])
+            y = int(keypoints[shortestDistanceIndex].pt[1])
 
-        k = cv2.waitKey(25) & 0xFF
-        if k == 27:
-            break
+            # Difference between the detection and the center, in pixels
+            diffX = x - center_x
+            diffY = y - center_y
+
+            # Difference between the detection and the center, in centimeters
+            # x: + up (egg is up relative to the center), - down
+            # y: + right, - left
+            altitude = 100
+            offsetX = distanceToRealWorld(altitude, diffX, center_x)
+            offsetY = distanceToRealWorld(altitude, diffY, center_y)
+
+            print "offsetX: {}     offsetY: {}".format(offsetX, offsetY)
+
+            # Draw centroids
+            for k in keypoints:
+                x = int(k.pt[0])
+                y = int(k.pt[1])
+                cv2.circle(img, (x, y), 10, (0, 0, 255), -1)
+
+            timer.sinceLastTimeLog('')
+            # cv2.imshow("filtered", f)
+            # cv2.imshow("mask", mask)
+            cv2.imshow("img_keypoints", img)
+
+            k = cv2.waitKey(25) & 0xFF
+            if k == 27:
+                break
 
     cv2.destroyAllWindows()
 
--- imageprocessing/pixelDistance.py
+++ imageprocessing/pixelDistance.py
@@ -1,5 +1,8 @@
 import math
 
+#Logitech C920 Pro. 16:9 aspect ratio. Degrees
+C920_DFOV_169 = 78
+
 def distanceBetweenPointsPixel(x1, y1, x2, y2):
     return math.sqrt(math.pow(x2-x1, 2) + math.pow(y2-y1, 2))
 
@@ -19,3 +22,21 @@ def distanceBetweenPointsGPS(lat1, lon1, lat2, lon2):
     d = R * c
 
     return d
+
+def distanceToRealWorld(altitude, distancePixel, halfWidth, camera = 'C920'):
+    if camera == 'C920':
+        dfov = C920_DFOV_169
+        aspect_ratio = 9/16
+    else:
+        dfov = 80
+        aspect_ratio = 9/16
+
+    dx = altitude
+    dh = dx / math.cos(math.radians((dfov/2)))
+    dy = dh * math.sin(math.radians(dfov/2))
+    alpha = math.atan(math.radians(aspect_ratio))
+    cx = dy * math.cos(alpha)
+    pixelRealRatio = cx/halfWidth
+    distanceReal = distancePixel * pixelRealRatio
+
+    return distanceReal
